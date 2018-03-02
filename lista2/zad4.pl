6faebfb727a7964787dba52637e29bcd7f66f40c
Mack13Podwin <maciej.podwinski@gmail.com>
2018-03-02 18:58:46 +0100

    Adding solution to 4 (not really working).

--- lista2/zad4.pl
+++ lista2/zad4.pl
@@ -1,4 +1,16 @@
-ma(0,X,Y):-ma(X,Y).
+%ma(Kiedy,Kto,Co):-
+%	ma(_,Co),
+%	ma(Kiedy,Kto,Co,[]).
+
+ma(0, Kto, Co):-
+	ma(Kto,Co).
+ma(Kiedy,Kto,Co):-
+	daje(Kiedy,_,Co,Kto).
+ma(Kiedy,Kto,Co):-
+	\+ daje(Kiedy,_,Co,Kto),
+	\+ daje(Kiedy, Kto, Co, _),
+	l(X,Kiedy),
+	ma(X,Kto,Co).
 
 ma(ala, książka).
 ma(jola, rower).
@@ -9,7 +21,7 @@ daje(potem(0),ala,książka,jola).
 daje(potem(0),jola,rower,ala).
 daje(potem(potem(0)),jola,książka,agata).
 
-potem(0).
-potem(potem(0)).
-potem(potem(X)):-
-	X=potem(_).
+l(0,potem(0)).
+l(potem(0),potem(potem(0))).
+
+
