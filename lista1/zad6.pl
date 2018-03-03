93996d480f937190593273b568682d872f3b6fac
Mack13Podwin <maciej.podwinski@gmail.com>
2018-03-03 14:46:53 +0100

    Better, but still not working correctly

--- lista1/zad6.pl
+++ lista1/zad6.pl
@@ -4,11 +4,11 @@ prime(LO,HI,N):-
 
 is_prime(2).
 is_prime(X):-
-	X<2,
-	fail.
-is_prime(X):-
 	X>2,
 	\+ divisible(X,2).
 
-divisible(X,Y):-0 is X mod Y.
-divisible(X,Y):-X > Y+1, divisible(X, Y+1).
+divisible(X,Y):-
+	0 is X mod Y;
+	X > Y+1,
+	Z is Y+1,
+	divisible(X, Z).
