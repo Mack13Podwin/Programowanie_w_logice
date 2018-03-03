823ab67a024b938314d293fbcc6b5e68bcf7cb21
Mack13Podwin <maciej.podwinski@gmail.com>
2018-03-03 14:37:21 +0100

    Better, but still not working correctly

--- lista2/zad5.pl
+++ lista2/zad5.pl
@@ -1,28 +1,27 @@
 lista(1,[1,1]).
-lista(N,[1|X]):-
-	numlist(2,N,Y),
-	lista(Y,Z,[],[1],1),
-	append(X,[],Z).
+lista(N,X):-
+	numlist(1,N,Y),
+	lista(Y,X,[],[],0).
 
 lista([],_,[],[],0):-
 	true.
 lista(IN,OUT,W0,W1,1):-
 	W1\=[],
 	select(Y,W1,Z),
-	append(OUT,[Y],OUT2),
+	append([Y],OUT2,OUT),
 	lista(IN,OUT2,W0,Z,0).
 lista([X|IN],OUT,W0,W1,1):-
-	append(OUT,[X],OUT2),
+	append([X],OUT2,OUT),
 	Y=[X|W0],
 	lista(IN,OUT2,Y,W1,0).
 
 lista(IN,OUT,W0,W1,0):-
 	W0\=[],
 	select(Y,W0,Z),
-	append(OUT,[Y],OUT2),
+	append([Y],OUT2,OUT),
 	lista(IN,OUT2,Z,W1,1).
 lista([X|IN],OUT,W0,W1,0):-
-	append(OUT,[X],OUT2),
+	append([X],OUT2,OUT),
 	Y=[X|W1],
 	lista(IN,OUT2,W0,Y,1).
 
