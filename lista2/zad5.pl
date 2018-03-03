62a89a8c0b2b1f706b438c723443b58d6e21c47f
Mack13Podwin <maciej.podwinski@gmail.com>
2018-03-03 14:14:10 +0100

    Failed implementation of my algorithm

--- lista2/zad5.pl
+++ lista2/zad5.pl
@@ -1,6 +1,28 @@
 lista(1,[1,1]).
 lista(N,[1|X]):-
 	numlist(2,N,Y),
-	append(X,[],Y).
+	lista(Y,Z,[],[1],1),
+	append(X,[],Z).
+
 lista([],_,[],[],0):-
 	true.
+lista(IN,OUT,W0,W1,1):-
+	W1\=[],
+	select(Y,W1,Z),
+	append(OUT,[Y],OUT2),
+	lista(IN,OUT2,W0,Z,0).
+lista([X|IN],OUT,W0,W1,1):-
+	append(OUT,[X],OUT2),
+	Y=[X|W0],
+	lista(IN,OUT2,Y,W1,0).
+
+lista(IN,OUT,W0,W1,0):-
+	W0\=[],
+	select(Y,W0,Z),
+	append(OUT,[Y],OUT2),
+	lista(IN,OUT2,Z,W1,1).
+lista([X|IN],OUT,W0,W1,0):-
+	append(OUT,[X],OUT2),
+	Y=[X|W1],
+	lista(IN,OUT2,W0,Y,1).
+
