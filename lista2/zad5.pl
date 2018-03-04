dcb875235da07b80f05e9492c55519abc8f79d1c
Mack13Podwin <maciej.podwinski@gmail.com>
2018-03-04 12:00:48 +0100

    Making zad5 faster

--- lista2/zad5.pl
+++ lista2/zad5.pl
@@ -1,27 +1,21 @@
-lista(1,[1,1]).
 lista(N,X):-
 	numlist(1,N,Y),
 	lista(Y,X,[],[],0).
 
-lista([],OUT,[],[],0):-
-	OUT=[].
+lista([],[],[],[],0).
 lista(IN,OUT,W0,W1,1):-
-	W1\=[],
 	select(Y,W1,Z),
-	append([Y],OUT2,OUT),
+	OUT=[Y|OUT2],
 	lista(IN,OUT2,W0,Z,0).
 lista([X|IN],OUT,W0,W1,1):-
-	append([X],OUT2,OUT),
-	Y=[X|W0],
-	lista(IN,OUT2,Y,W1,0).
+	OUT=[X|OUT2],
+	lista(IN,OUT2,[X|W0],W1,0).
 
 lista(IN,OUT,W0,W1,0):-
-	W0\=[],
 	select(Y,W0,Z),
-	append([Y],OUT2,OUT),
+	OUT=[Y|OUT2],
 	lista(IN,OUT2,Z,W1,1).
 lista([X|IN],OUT,W0,W1,0):-
-	append([X],OUT2,OUT),
-	Y=[X|W1],
-	lista(IN,OUT2,W0,Y,1).
+	OUT=[X|OUT2],
+	lista(IN,OUT2,W0,[X|W1],1).
 
