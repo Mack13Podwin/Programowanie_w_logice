3cf6c5936aad2e87ccafe022448e86d1479cbaef
Mack13Podwin <maciej.podwinski@gmail.com>
2018-05-19 22:19:16 +0200

    Adding better 1 and 2

--- lista7/zad1.pl
+++ lista7/zad1.pl
@@ -1,23 +1,12 @@
-merge(In1,In2,Out):-
-	freeze(In1,(freeze(In2,(
-			      In1=[H1|T1],
-			      In2=[H2|T2],
-			      (	  H1=<H2->(
-				      Out=[H1|NOut],
-				      merge(T1,In2,NOut));
-			      (
-				      Out=[H2|NOut],
-				      merge(In1,T2,NOut))),!
-			  );
-			 (
-			      In1=[H1|T],
-			      Out=[H1|NOut],
-			      merge(T,In2,NOut));
-			 (
-			     In2=[H2|T],
-			     Out=[H2|NOut],
-			     merge(In1,T,NOut))),!);
-	       Out=[]),!.
+mymerge(In1,In2,Out):-
+	freeze(In1,(freeze(In2,merge2(In1,In2,Out)))).
+
+merge2([],In2,In2):-!.
+merge2(In1,[],In1):-!.
+merge2(In1,In2,Out):-
+	In1=[H1|T1],In2=[H2|T2],
+	(   (H1=<H2)->Out=[H1|NOut],mymerge(T1,In2,NOut);
+	Out=[H2|NOut],mymerge(In2,T2,NOut)).
 
 
 
--- lista7/zad2.pl
+++ lista7/zad2.pl
@@ -14,13 +14,14 @@ split(In,Out1,Out2,2):-
 	      split(NIn,Out1,NOut2,1))),!;
 	Out1=[],Out2=[].
 
-mergesort([],[]).
-mergesort([X],[X]):-freeze(X,true).
-mergesort(In,Out):-
-	freeze(In,(
-	       split(In,Left,Right),
-	       mergesort(Left,S1),
-	       mergesort(Right,S2),
-	       merge(S1,S2,Out))).
+merge_sort(In,Out):-freeze(In, merge_sort2(In,Out)).
+
+merge_sort2([],[]).
+merge_sort2([H|T],Out):-freeze(T,
+			       (T=[])->(Out=[H]);
+			       (split([H|T],Out1,Out2),
+				merge_sort(Out1,Out11),
+				merge_sort(Out2,Out22),
+				mymerge(Out11,Out22,Out))).
 
 
