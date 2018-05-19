0ea30afd62c6d601d9ad9b170c0fe480ab56ccba
Mack13Podwin <maciej.podwinski@gmail.com>
2018-05-19 20:32:36 +0200

    Adding working split

--- lista7/zad2.pl
+++ lista7/zad2.pl
@@ -1,13 +1,17 @@
 :-consult('zad1.pl').
 
-split(In,Out1,Out2):-
+split(In,Out1,Out2):-split(In,Out1,Out2,1).
+split(In,Out1,Out2,1):-
 	freeze(In,(
-	       In=[X,Y|NIn],
+	       In=[X|NIn],
 	       Out1=[X|NOut1],
-	       Out2=[Y|NOut2],
-	       split(NIn,NOut1,NOut2));
-	       (
-		      In=[X|NIn],
-		      Out1=[X|NOut1],
-		      split(NIn,NOut1,Out2))),!;
+	       split(NIn,NOut1,Out2,2))),!;
 	Out1=[],Out2=[].
+split(In,Out1,Out2,2):-
+	freeze(In,(
+	      In=[X|NIn],
+	      Out2=[X|NOut2],
+	      split(NIn,Out1,NOut2,1))),!;
+	Out1=[],Out2=[].
+
+
