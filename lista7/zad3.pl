fc2f9f00d2eaadfa3c28aa7312e911653ed8f33e
Mack13Podwin <maciej.podwinski@gmail.com>
2018-05-26 12:33:40 +0200

    Adding: fixed z3 l7

--- lista7/zad3.pl
+++ lista7/zad3.pl
@@ -11,34 +11,27 @@ filozofowie:-
 	thread_join(ID5,_).
 
 życie(ID):-
-	myśli(ID),
 	(   ID<5->Left is ID,Right is ID+1;Left is 5, Right is 1),
-	napisz(ID,'chce prawy widelec'),
-	podnieś_widelec(ID,Right,p),
-	napisz(ID,'chce lewy widelec'),
-	podnieś_widelec(ID,Left,l),
-	je(ID),
-	odkłada_widelec(ID,Right,p),
-	odkłada_widelec(ID,Left,l),
+	atom_concat('widelec',Right,MR),
+	mutex_lock(MR),
+	atom_concat('widelec',Left,ML),
+	(
+	    mutex_trylock(ML)->
+	    napisz(ID,'myśli'),
+	    napisz(ID,'chce prawy widelec'),
+	    napisz(ID,'podniósł prawy widelec'),
+	    napisz(ID,'chce lewy widelec'),
+	    napisz(ID,'podniósł lewy widelec'),
+	    napisz(ID,'je'),
+	    napisz(ID,'odkłada prawy widelec'),mutex_unlock(MR),
+	    napisz(ID,'odkłada lewy widelec'), mutex_unlock(ML)
+	    ;
+	    mutex_unlock(MR)
+	),
+	sleep(1),
 	życie(ID).
 
-myśli(ID):-
-	napisz(ID,"myśli"),
-	sleep(ID).
 
-podnieś_widelec(ID,W,K):-
-	atom_concat('widelec',W,M),
-	mutex_lock(M),
-	(   K=p->napisz(ID,'podniósł prawy widelec');napisz(ID,'podniósł lewy widelec')).
-
-je(ID):-
-	napisz(ID,"je"),
-	sleep(ID).
-
-odkłada_widelec(ID,W,K):-
-	atom_concat('widelec',W,M),
-	mutex_unlock(M),
-	(   K=p->napisz(ID,'odkłada prawy widelec');napisz(ID,'odkłada lewy widelec')).
 
 napisz(ID,STRING):-
 	mutex_lock(pisanie),
